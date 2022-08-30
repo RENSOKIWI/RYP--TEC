1a03b1fa9512ad16cf98157a531e318247d108a6
RENSOKIWI <112492534+RENSOKIWI@users.noreply.github.com>
2022-08-30 07:58:09 -0600

    Add files via upload

---
+++ Tienda/fe/paginas/boton.js
@@ -0,0 +1,15 @@
+
+
+    (function () {
+        var options = {
+            facebook: "100084526586803", // Facebook page ID
+            call_to_action: "Contactanos", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+
